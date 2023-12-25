b60194bc5b5e31ea75da44633a97718d7c20bee2
Sasank <sasank.desaraju@ufl.edu>
2023-12-26 00:46:17 +0530

    CLI is running! Just needed pip...

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from lightning import LightningModule
 from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.demos.boring_classes import DemoModel, BoringDataModule
+
+def cli_main():
+    cli = LightningCLI(DemoModel, BoringDataModule, seed_everything_default=42)
+    #cli.trainer.tune(cli.model, datamodule=cli.datamodule)
+
+if __name__ == '__main__':
+    cli_main()
