28c8022c18b7814da8d675431c04dd2033543c90
Sasank Desaraju <sasank.desaraju@ufl.edu>
2024-04-17 21:51:28 -0400

    moved dev code, edited train.py, and edited README

--- README.md
+++ README.md
@@ -45,3 +45,25 @@ Example of workflow for medical AI training and testing
 - PyTorch Lightning to eliminate as much boilerplate code as posible.
 - Possibly use MONAI library for their well-built ecosystem. I don't know how to work it super well yet, though.
 - Config using YAML files somehow. I am playing around with Lightning CLI to make it easy to switch between train and test but I'm not sure I like how abstracted away it feels.
+
+
+## Next Steps:
+- Put the Dataset into the Datamodule so that there is only one file that needs changing with a different dataset.
+    - Make sure that the Datamodule can be easily played around with in a Notebook for development purposes. Maybe best is the same as now where you copy/paste the Notebook Dataset in? Idk
+- Should I rename ./data/ ? Rn it's confusing bc there's also ./src/data/ which is for source code...
+- Achieve a fast_dev_run with the Hydra config system
+- Get config/, models/, data/, and splits/.
+    - I guess the config would need to specify the split used, but all splits would need to have the form so that the same Dataset/Datamodule can use them.
+- Then, work on logging. We want to use MLFlow to log to, potentially, a common logging directory.
+    - Use Hydra tags to automatically tag each run with the $USER that ran it. This will be important if a common logging directory is used.
+- Logging framework should include local environment variables I think. Which local environment variables, though? Like their home directory or so? Maybe device=HPG or device=Local depending on HPG run vs. their local machine??
+- Then, tests.
+    - Would be nice to have tests verify that everything exists and instantiates properly. I think the tests they have in the template repo are pretty solid to get started.
+    - Maybe first as an "extra" that is run before every run? Need to see how much time it will take to run lol
+
+- Need to have some visualization stuff.
+    - For visualizing results, for example. Some notebooks. But maybe some that's auto-run during eval.py?
+- Need to have some built out pipeline for making the datamodule and making sure everything with the data is working as expected.
+    - Like notebooks that pull from your config/config.yaml and src/data/my_datamodule.py so that they basically help you build it.
+
+
---
+++ src/train.py
@@ -0,0 +1,122 @@
+from typing import Any, Dict, List, Optional, Tuple
+
+import hydra
+import lightning as L
+import rootutils
+import torch
+from lightning import Callback, LightningDataModule, LightningModule, Trainer
+from lightning.pytorch.loggers import Logger
+from omegaconf import DictConfig
+
+from src.utils import (
+    RankedLogger,
+    extras,
+    get_metric_value,
+    instantiate_callbacks,
+    instantiate_loggers,
+    log_hyperparameters,
+    task_wrapper,
+)
+
+log = RankedLogger(__name__, rank_zero_only=True)
+
+
+def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    """Trains the model. Can additionally evaluate on a testset, using best weights obtained during
+    training.
+
+    This method is wrapped in optional @task_wrapper decorator, that controls the behavior during
+    failure. Useful for multiruns, saving info about the crash, etc.
+
+    :param cfg: A DictConfig configuration composed by Hydra.
+    :return: A tuple with metrics and dict with all instantiated objects.
+    """
+    # set seed for random number generators in pytorch, numpy and python.random
+    # I want to set the seed to a fixed value. Oh, I think this is using the cfg.seed value.
+    if cfg.get("seed"):
+        L.seed_everything(cfg.seed, workers=True)
+
+    log.info(f"Instantiating datamodule <{cfg.data._target_}>")
+    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
+
+    log.info(f"Instantiating model <{cfg.model._target_}>")
+    model: LightningModule = hydra.utils.instantiate(cfg.model)
+
+    log.info("Instantiating callbacks...")
+    callbacks: List[Callback] = instantiate_callbacks(cfg.get("callbacks"))
+
+    log.info("Instantiating loggers...")
+    logger: List[Logger] = instantiate_loggers(cfg.get("logger"))
+
+    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
+
+    # All options are set by this point it seems
+
+    object_dict = {
+        "cfg": cfg,
+        "datamodule": datamodule,
+        "model": model,
+        "callbacks": callbacks,
+        "logger": logger,
+        "trainer": trainer,
+    }
+
+    if logger:
+        log.info("Logging hyperparameters!")
+        log_hyperparameters(object_dict)
+
+    if cfg.get("train"):        # Is this syntax returning a Boolean? I think so. I need to look into the Hydra syntax more.
+        log.info("Starting training!")
+        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+
+    train_metrics = trainer.callback_metrics
+
+    if cfg.get("test"):     # This is a test call but is in the train.py file. That's interesting... Will need to see how I feel about this.
+        log.info("Starting testing!")
+        ckpt_path = trainer.checkpoint_callback.best_model_path
+        if ckpt_path == "":
+            log.warning("Best ckpt not found! Using current weights for testing...")
+            ckpt_path = None
+        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+        log.info(f"Best ckpt path: {ckpt_path}")
+
+    test_metrics = trainer.callback_metrics
+
+    # merge train and test metrics
+    metric_dict = {**train_metrics, **test_metrics}
+
+    return metric_dict, object_dict
+
+
+@hydra.main(version_base="1.3", config_path="../config", config_name="default_train.yaml")
+def main(cfg: DictConfig) -> Optional[float]:
+    """Main entry point for training.
+
+    :param cfg: DictConfig configuration composed by Hydra.
+    :return: Optional[float] with optimized metric value.
+    """
+
+    train(cfg)
+
+    # What is the below stuff lol
+    """
+    # apply extra utilities
+    # (e.g. ask for tags if none are provided in cfg, print cfg tree, etc.)
+    extras(cfg)
+
+    # train the model
+    metric_dict, _ = train(cfg)
+
+    # safely retrieve metric value for hydra-based hyperparameter optimization
+    metric_value = get_metric_value(
+        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
+    )
+
+    # return optimized metric
+    return metric_value
+    """
+
+
+if __name__ == "__main__":
+    main()
